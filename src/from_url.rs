824f7758621297b8596dd2056e4c19be72ac3a0f
JohannesIBK <mail@greuter.dev>
2022-11-22 19:31:25 +0100

    use attohttpc instead of reqwest and update dependencies

--- src/from_url.rs
+++ src/from_url.rs
@@ -5,8 +5,8 @@ pub fn download_image(url: &str) -> Result<String> {
     let cache_dir = dirs::cache_dir().ok_or("no cache dir")?;
     let file_path = cache_dir.join("wallpaper");
 
-    let mut file = File::create(&file_path)?;
-    reqwest::blocking::get(url)?.copy_to(&mut file)?;
+    let file = File::create(&file_path)?;
+    attohttpc::get(url).send()?.write_to(file)?;
 
     Ok(file_path.to_str().to_owned().ok_or("no file path")?.into())
 }
